338aca81c0b7d882718ab3080500dcea35e3f2a9
hsweetlin <sweetlinjuliet112001@gmail.com>
2024-01-26 19:01:19 +0530

    update: make additional changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,32 +3,33 @@ const fs = require('fs');
 const url = require('url');
 const nodemailer = require('nodemailer');
 var mysql = require('mysql');
-if (dotenv) dotenv.config();
+// if (dotenv) dotenv.config();
 var config = require('./config');
 
+var port = config.port;
+var host = config.host
+// var transporter = nodemailer.createTransport({
+//   service : 'gmail',
+//   auth : {
+//     user: config.mail.mail_id,
+//     pass: config.mail.mail_password
+//   }
+// })
 
-var transporter = nodemailer.createTransport({
-  service : 'gmail',
-  auth : {
-    user: config.mail.mail_id,
-    pass: config.mail.mail_password
-  }
-})
+//   var mailOptions = {
+//     from: config.mail.mail_id,
+//     to: 'xxx',
+//     subject: 'Sending email using nodemailer',
+//     text: 'Easy to use'
+//   }
 
-  var mailOptions = {
-    from: config.mail.mail_id,
-    to: 'xxx',
-    subject: 'Sending email using nodemailer',
-    text: 'Easy to use'
-  }
-
-  transporter.sendMail(mailOptions, function(error,info){
-if(error){
-  console.log(error)
-} else{
-  console.log('Email sent' + info.response)
-}
-  })
+//   transporter.sendMail(mailOptions, function(error,info){
+// if(error){
+//   console.log(error)
+// } else{
+//   console.log('Email sent' + info.response)
+// }
+//   })
 
 
 // var readStream = fs.createReadStream('./demo.html');
@@ -72,7 +73,7 @@ const server = http.createServer((req, res) => {
     
 
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${config.host}:${config.port}/`);
+server.listen(port, () => {
+  console.log(`Server running at https://${host}:${port}`);
 });
 
